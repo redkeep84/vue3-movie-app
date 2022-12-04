9e23c25ddbfa3e0c391921e887d69404dde391aa
redkeep84 <ckdfud@gmail.com>
2022-12-04 22:28:21 +0900

    Start Project

---
+++ src/components/Logo.vue
@@ -0,0 +1,24 @@
+<template>
+  <RouterLink 
+    to="/" 
+    class="log">
+    <span>OMDbAPI</span>.com
+  </RouterLink>
+</template>
+
+<style lang="scss" scoped>
+@import "~/scss/main";
+
+.log {
+  font-family: "Oswald", sans-serif;
+  font-size: 20px;
+  color: $black;
+  text-decoration: none;
+  &:hover {
+    color: $black;
+  }
+  span {
+    color: $primary;
+  }
+}
+</style>
